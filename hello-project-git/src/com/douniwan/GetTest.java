8510637e9b2f93209e10a48cb8a3ab98d7132c8b
Mr.ding <dingshuangshuang@163.com>
2018-10-29 15:14:41 +0800

    把本地工程添加到本地仓库

---
+++ hello-project-git/src/com/douniwan/GetTest.java
@@ -0,0 +1,10 @@
+package com.douniwan;
+
+public class GetTest {
+    public static void main(String[] args) {
+        System.out.println("hello project-git");
+        System.out.println("settings中的version control中的git配置一下路径");
+        System.out.println("发送到本地仓库 点击 vcs import into version control-->create git repository" +
+                "选 ideal project  ");
+    }
+}
